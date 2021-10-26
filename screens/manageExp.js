e112d4eee7896d8e6e8902d446f4fea8de54a6bf
sminy85 <smini857@naver.com>
2021-10-26 18:18:34 +0900

    manage expired ingredients page

--- screens/manageExp.js
+++ screens/manageExp.js
@@ -22,10 +22,14 @@ export default function manageExp () {
   const [selectAll, setSelectAll] = React.useState(false);
   const [cartItemIsLoading, setCartItemIsLoading] = React.useState(false);
   const [unchecked, setUnchecked] = React.useState([]);
+  const [date, setDate] = useState(new Date());
+  const [counting, setCounting] = React.useState(0);
+
   const id = useSelector((state) => state.id);
   React.useEffect(()=>{
     axios.get(`${API_URL}/manage?user_id=${id}`).then((result)=>{
       setIngredients(result.data);
+      console.log(result.data);
     }).catch((error)=>{
       console.error(error);
     })
@@ -43,42 +47,69 @@ export default function manageExp () {
   // }, [ingredients]);
 
   React.useEffect(() => {
-    ingredients.map((ing)=> {
-      let tempData = {id: 1, name: ing.ing.ing_name, checked: 0};
-      setInsertData(prev => [...prev, tempData]);
-      console.log("tempData",tempData);
-      console.log('insssss', insertData);
+    ingredients.map((ing) => {
+      // ing.ing_expir = changeDateFormat(ing.ing_expir);
+      if(ing.ing_expir < date) {
+        ing.ing_expir = changeDateFormat(ing.ing_expir);
+        let tempData = {id: ing._id, expir: ing.ing_expir, name: ing.ing.ing_name, checked: 0};
+        setInsertData(prev => [...prev, tempData]);
+      }
     });
   }, [ingredients]);
+
+  const changeDateFormat = (oldDate) => {
+    if(oldDate === null) {
+      return '2021-01-01'
+    }
+    let newDate = oldDate.substr(0, 10);
+    return newDate
+  }
 
   //const data = [{id: 1, name: '계란'}, {id: 2, name: '토마토'}, {id: 3, name: '오이'}];
 	const selectHandler = (index, value) => {
 		const newItems = [...insertData]; // clone the array 
 		newItems[index]['checked'] = value == 1 ? 0 : 1; // set the new value 
-		setInsertData(newItems); // set new state
+    setInsertData(newItems); // set new state
+
+    if(newItems[index].checked == 1){
+      setCounting(counting + 1);
+      console.log(counting);
+    } else if(newItems[index].checked == 0){
+      setCounting(counting - 1);
+    }
 	}
 	
 	const selectHandlerAll = (value) => {
 		const newItems = [...insertData]; // clone the array 
 		newItems.map((item) => {
-			item['checked'] = value == true ? 0 : 1; // set the new value 
+      item['checked'] = value == true ? 0 : 1; // set the new value 
+      if(newItems[index].checked == 1){
+        setCounting(counting + 1);
+      } else if(newItems[index].checked == 0){
+        setCounting(counting - 1);
+      }
 		});
     //this.setState({ cartItems: newItems, selectAll: (value == true ? false : true) }); // set new state
     setInsertData(newItems);
     setSelectAll(value == true ? false : true);
 	}
 	
-	const deleteHandler = (index) => {
+	const deleteHandler = (idd,index) => {
 		Alert.alert(
 			'관리 페이지에서 정말 삭제하시겠습니까?',
 			'',
 			[
 				{text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
 				{text: 'Delete', onPress: () => {
-          const updatedCart = [...insertData]; /* Clone it first */
-          // console.log('delete'+updatedCart);
-					updatedCart.splice(index, 1); /* Remove item from the cloned cart state */
-					setInsertData(updatedCart); /* Update the state */
+          axios.delete(`${API_URL}/manage`, {data:{ _id: idd }})
+          .then((res) => {
+            alert("삭제완료");
+            const updatedCart = [...insertData];
+            updatedCart.splice(index, 1);
+            setInsertData(updatedCart);
+          }).catch(error => {
+            console.log(error);
+          })
 				}},
 			],
 			{ cancelable: false }
@@ -87,6 +118,7 @@ export default function manageExp () {
   
   const deleteSelectedHandler = () => {
     const deleteSelected = [...insertData];
+    console.log(deleteSelected);
 		Alert.alert(
 			'선택 항목을 관리 페이지에서 정말 삭제하시겠습니까?',
 			'',
@@ -94,10 +126,19 @@ export default function manageExp () {
 				{text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
 				{text: 'Delete', onPress: () => {
           deleteSelected.map((item, index) => {
-            if(deleteSelected[index]['checked'] === false)
-              unchecked = [...deleteSelected[index]]
+            if(item['checked'] === 1){
+              console.log(item.id);
+              axios.delete(`${API_URL}/manage`, {data:{ _id: item.id }})
+              .then((res) => {
+                alert("삭제완료");
+                const updatedCart = [...insertData];
+                updatedCart.splice(index, 1);
+                setInsertData(updatedCart);
+              }).catch(error => {
+                console.log(error);
+              })
+            }
           });
-          setInsertData(unchecked); /* Update the state */
 				}},
       ],
 			{ cancelable: false }
@@ -155,15 +196,15 @@ export default function manageExp () {
               </View>
             </View>
             <View style={[styles.centerElement, {width: 60}]}>
-              <TouchableOpacity style={[styles.centerElement, {width: 32, height: 32}]} onPress={() => deleteHandler(i)}>
+              <TouchableOpacity style={[styles.centerElement, {width: 32, height: 32}]} onPress={() => deleteHandler(item.id, i)}>
                 <Ionicons name="md-trash" size={25} color="#ee4d2d" />
               </TouchableOpacity>
             </View>
           </View>
         ))}
       </ScrollView>
-      <TouchableOpacity style={styles.cartBtn} onPress={() => deleteSelectedHandler()}>
-          <Ionicons name="basket-outline" size={30} color="white" />
+      <TouchableOpacity style={[styles.cartBtn , {backgroundColor : counting > 0 ? '#f9b664' : '#fcd9ae'}]} onPress={() => deleteSelectedHandler()}>
+          <Ionicons name="basket-outline" size={30} color='white' />
           <Text style={{ color: 'white', fontSize: 18 }}> 장바구니에 담기</Text>
         </TouchableOpacity>
     </View>
@@ -196,6 +237,6 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     width: Dimensions.get('screen').width,
     height: Dimensions.get('screen').width*0.12,
-    backgroundColor: '#fcd9ae',
+    //backgroundColor: '#fcd9ae',
   }
 });
